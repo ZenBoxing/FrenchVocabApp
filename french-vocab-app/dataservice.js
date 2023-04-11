4a73dcb5c3819c6ba890720e1a0ca9dfb3e8d7f7
Jarrett Lirette <jarrettlirette@gmail.com>
2023-04-11 19:06:39 -0400

    Deleted xlsb file, Completed dataservice.js

--- french-vocab-app/dataservice.js
+++ french-vocab-app/dataservice.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
 
 let filename = 'C:\\Users\\jarre\\WebApps\\FrenchVocabApp\\french-vocab-app\\Fre.Freq.3.Hun.txt';
+let wordList = [];
+let dataArr = [];
 
-async function loadFile()
+function loadFile()
 {
-    
-}
+    fs.readFile(filename, (err, data) => {
+        if(err) throw err;
 
+        dataArr = data.toString().split("\n");
+    });
+
+    for(let i = 0; i < dataArr.length; i++){
+            
+        let line = dataArr[i].split("\t");
+
+        wordList[i] = {
+            word : line[0],
+            blogFreq : line[1],
+            newsFreq : line[10]
+        }
+   }
+
+    return wordList;
+}
 
 loadFile();
