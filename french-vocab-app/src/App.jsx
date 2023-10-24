2d93a809ded22953971ec233c867b78ab237f0f5
Jarrett Lirette <jarrettlirette@gmail.com>
2023-10-23 20:37:59 -0400

    Pass data from txt.File to VocabListExtimator

--- french-vocab-app/src/App.jsx
+++ french-vocab-app/src/App.jsx
@@ -5,16 +5,38 @@ import raw from  './data/Fre.Freq.3.Hun.txt'
 
 const App = () => {
 
+  let words;
 
    fetch(raw)
     .then(r => r.text())
     .then(text => {
-       console.log('text decoded:', text);
-     });
+       //console.log('text decoded:', text);
+      
+       const dataArr = text.split("\n");
+       const wordList = [];
+
+       for(let i = 0; i < 15; i++){
+            
+        let line = dataArr[i].split("\t");
+
+        wordList[i] = {
+            word : line[0],
+            blogFreq : line[1],
+            newsFreq : line[10]
+        }        
+    }
+
+    for(let i = 0; i < wordList.length; i++){
+      console.log(wordList[i].word);
+    }
+
+    words = wordList;
+
+ });
 
   return (
     <div className="App">
-        <VocabEstimator/>
+        <VocabEstimator wordlist={words}/>
     </div>
   );
 }
