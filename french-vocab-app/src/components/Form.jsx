48c2b5b3a46cddd7dfcdbf5a8f561e64fafa3775
Jarrett Lirette <jarrettlirette@gmail.com>
2023-11-30 19:22:11 -0500

    Added more state variables for word arrays. Created SetList function to fill arrays. Revised onGenerateForm function

--- french-vocab-app/src/components/Form.jsx
+++ french-vocab-app/src/components/Form.jsx
@@ -4,7 +4,14 @@ import raw from  './componentdata/Fre.Freq.3.Hun.txt';
 
 const Form = () => {
 
-     const [words, setWords] = useState([]);
+     const [levelOne, setLevelOne] = useState([]);
+     const [levelTwo, setLevelTwo] = useState([]);
+     const [levelThree, setLevelThree] = useState([]);
+     const [levelFour, setLevelFour] = useState([]);
+     const [levelFive, setLevelFive] = useState([]);
+     const [levelSix, setLevelSix] = useState([]);
+     const [levelSeven, setLevelSeven] = useState([]);
+
      const [checkedList, setCheckedList] = useState([]);
 
      const onGenerateForm = () => {
@@ -13,26 +20,39 @@ const Form = () => {
             .then(response => response.text()) 
             .then(textString => {
         
-            let dataArr = textString.split("\n");
-            let list = [];
-
-            //array length of five for now
-            for(let i = 1; i < 70; i++){
-              
-
-              let num = Math.floor(getRandomArbitrary(0, 3000));
-              let line = dataArr[num].split("\t");
-      
-              list[i] = {
-                  word : line[0],
-                  blogFreq : line[1],
-                  newsFreq : line[10]
-              }
-        }        
-          setWords(list);      
+            let dataArray = textString.split("\n");
+            
+            setList(dataArray, 0, 3000, setLevelOne);
+            setList(dataArray, 3000, 12000, setLevelTwo);
+            setList(dataArray, 12000, 20000, setLevelThree);
+            setList(dataArray, 20000, 30000, setLevelFour);
+            setList(dataArray, 30000, 35000, setLevelFive);
+            setList(dataArray, 35000, 40000, setLevelSix);
+            setList(dataArray, 40000, 45000, setLevelSeven);
+                  
+            
         });
      }
 
+     const setList = (array, min, max, setter) => {
+      
+        let list = [];
+       
+        for(let i = 1; i < 11; i++){
+              
+
+          let num = Math.floor(getRandomArbitrary(min, max));
+          let line = array[num].split("\t");
+  
+          list[i] = {
+              word : line[0],
+              blogFreq : line[1],
+              newsFreq : line[10]
+          }
+       }  
+              
+      setter(list);      
+     }
 
      const getRandomArbitrary = (min, max) => {
         return Math.random() * (max - min) + min;
@@ -63,7 +83,7 @@ const Form = () => {
          <h2>form</h2>
          <button onClick={onGenerateForm}>Generate Form</button>
          <button onClick={onCompleteForm}>Submit</button>
-         {words.map((word, index) => {
+         {levelOne.map((word, index) => {
             return(
               <div key={word.word} className="checkbox-container">
                 <label>{word.word}</label>
@@ -77,43 +97,6 @@ const Form = () => {
          })}
       </div>
     );
-    // const [checkedState, setCheckedState] = useState(
-    //   new Array(wl.length).fill(false)
-    // );
-
-    // const handleOnChange = (position) => {
-    //   const updatedCheckedState = checkedState.map((item, index) =>
-    //     index === position  ? !item : item
-    //   );
-
-    //   setCheckedState(updatedCheckedState);
-    // }
-
-    // return(
-    //   <div classname="vocab-form">
-    //     <ul className="word-list">
-    //     {wl.map(({ word }, index) => {
-    //       return (
-    //         <li key={index}>
-    //           <div className="word-list-item">
-    //             <div className="left-section">
-    //               <input
-    //                 type="checkbox"
-    //                 id={`custom-checkbox-${index}`}
-    //                 name={word}
-    //                 value={word}
-    //                 checked={checkedState[index]}
-    //                 onChange={() => handleOnChange(index)}
-    //               />
-    //               <label htmlFor={`custom-checkbox-${index}`}>{word}</label>
-    //             </div>
-    //            </div>
-    //         </li>
-    //       );
-    //     })}
-    //   </ul>
-    //   </div>
-    // );
 
 }
 
