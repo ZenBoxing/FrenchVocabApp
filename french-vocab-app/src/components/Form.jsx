be573cfc979a1f2edbf1cabf0f22560f06d0a2ba
Jarrett Lirette <jarrettlirette@gmail.com>
2023-12-04 19:02:48 -0500

    Worked on Form.jsx
    added rank to words element fields,
    removed newsFreq and blogFreq
    from words element fields

--- french-vocab-app/src/components/Form.jsx
+++ french-vocab-app/src/components/Form.jsx
@@ -6,11 +6,11 @@ const Form = () => {
 
      const [levelOne, setLevelOne] = useState([]);
      const [levelTwo, setLevelTwo] = useState([]);
-     const [levelThree, setLevelThree] = useState([]);
-     const [levelFour, setLevelFour] = useState([]);
-     const [levelFive, setLevelFive] = useState([]);
-     const [levelSix, setLevelSix] = useState([]);
-     const [levelSeven, setLevelSeven] = useState([]);
+    //  const [levelThree, setLevelThree] = useState([]);
+    //  const [levelFour, setLevelFour] = useState([]);
+    //  const [levelFive, setLevelFive] = useState([]);
+    //  const [levelSix, setLevelSix] = useState([]);
+    //  const [levelSeven, setLevelSeven] = useState([]);
 
      const [checkedList, setCheckedList] = useState([]);
 
@@ -24,11 +24,11 @@ const Form = () => {
             
             setList(dataArray, 0, 3000, setLevelOne);
             setList(dataArray, 3000, 12000, setLevelTwo);
-            setList(dataArray, 12000, 20000, setLevelThree);
-            setList(dataArray, 20000, 30000, setLevelFour);
-            setList(dataArray, 30000, 35000, setLevelFive);
-            setList(dataArray, 35000, 40000, setLevelSix);
-            setList(dataArray, 40000, 45000, setLevelSeven);
+            // setList(dataArray, 12000, 20000, setLevelThree);
+            // setList(dataArray, 20000, 30000, setLevelFour);
+            // setList(dataArray, 30000, 35000, setLevelFive);
+            // setList(dataArray, 35000, 40000, setLevelSix);
+            // setList(dataArray, 40000, 45000, setLevelSeven);
                   
             
         });
@@ -43,11 +43,11 @@ const Form = () => {
 
           let num = Math.floor(getRandomArbitrary(min, max));
           let line = array[num].split("\t");
-  
+          
+          //possibly dont need bloqfreq or newsFreq
           list[i] = {
               word : line[0],
-              blogFreq : line[1],
-              newsFreq : line[10]
+              rank : num
           }
        }  
               
@@ -84,6 +84,19 @@ const Form = () => {
          <button onClick={onGenerateForm}>Generate Form</button>
          <button onClick={onCompleteForm}>Submit</button>
          {levelOne.map((word, index) => {
+            return(
+              <div key={word.word} className="checkbox-container">
+                <label>{word.word}</label>
+                <input
+                  type="checkbox"
+                  value={word.word}
+                  onChange={handleSelect}
+                />
+              </div>
+            )
+         })}
+
+         {levelTwo.map((word, index) => {
             return(
               <div key={word.word} className="checkbox-container">
                 <label>{word.word}</label>
